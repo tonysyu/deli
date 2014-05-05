396f6439d6fe62b69efa5e3443e12c2b3462e821
Tony S Yu <tsyu80@gmail.com>
2014-05-05 01:47:46 -0500

    Add demo of a custom tool

--- simple_plot.py
+++ simple_plot.py
@@ -3,7 +3,7 @@ from scipy.special import jn
 
 from enable.api import Component, ComponentEditor
 from traits.api import HasStrictTraits, Instance
-from traitsui.api import Item, Group, View
+from traitsui.api import UItem, View
 
 from deli.plot import Plot
 from deli.tools.pan_tool import PanTool
@@ -15,24 +15,16 @@ class Demo(HasStrictTraits):
     plot = Instance(Component)
 
     traits_view = View(
-        Group(
-            Item(
-                'plot',
-                editor=ComponentEditor(size=(900, 500)),
-                show_label=False
-            ),
-            orientation='vertical'),
+        UItem('plot', editor=ComponentEditor(size=(900, 500))),
         resizable=True, title="Basic x-y plots"
     )
 
     def _plot_default(self):
-        # Create some x-y data series to plot
         x = linspace(-2.0, 10.0, 100)
         pd = NoisyDict(x=x)
         for i in range(5):
             pd['y' + str(i)] = jn(i, x)
 
-        # Create some line plots of some of the data
         plot = Plot(pd, title="Line Plot")
         plot.plot(('x', 'y0', 'y1', 'y2'), color='red')
         plot.plot(('x', 'y3'), color='blue')
---
+++ tool_demo.py
@@ -0,0 +1,67 @@
+from numpy import linspace
+
+from enable.api import Component, ComponentEditor
+from traits.api import HasStrictTraits, Instance
+from traitsui.api import UItem, View
+
+from deli.plot import Plot
+from deli.tools.base_tool import BaseTool, BaseToolState
+from deli.tools.key_spec import KeySpec, shift_key
+from deli.utils.data_structures import NoisyDict
+
+
+class PrintTool(BaseTool):
+    """ Tool that prints current mouse position if the shift-key is pressed.
+
+    This tool delegates the actual action to PrintOnMove.
+    """
+
+    print_key = shift_key
+
+    def _state_handlers_default(self):
+        return {'print': PrintOnMove(self, enabling_key=self.print_key)}
+
+    def on_key_pressed(self, event):
+        if self.print_key.match(event):
+            self.state_change(event, new_state='print')
+
+
+class PrintOnMove(BaseToolState):
+    """ Tool state that prints the current position when the mouse is moved.
+
+    Note that this class returns control to the parent tool when the enabling
+    key is released.
+    """
+
+    enabling_key = Instance(KeySpec)
+
+    def on_mouse_move(self, event):
+        print "Mouse at ({}, {})".format(event.x, event.y)
+
+    def on_key_released(self, event):
+        if self.enabling_key.match(event):
+            self.exit_state(event)
+
+
+class Demo(HasStrictTraits):
+    plot = Instance(Component)
+
+    traits_view = View(
+        UItem('plot', editor=ComponentEditor(size=(900, 500))),
+        resizable=True, title="Basic x-y plots"
+    )
+
+    def _plot_default(self):
+        x = y = linspace(0, 1)
+        pd = NoisyDict(x=x, y=y)
+
+        plot = Plot(pd, title="Line Plot")
+        plot.plot(('x', 'y'))
+
+        PrintTool.attach_to(plot)
+        return plot
+
+
+if __name__ == '__main__':
+    demo = Demo()
+    demo.configure_traits()
