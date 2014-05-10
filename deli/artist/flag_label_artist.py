56636e62297706f8f95a1281d439f5ee60a9f28c
Tony S Yu <tsyu80@gmail.com>
2014-05-10 00:36:04 -0500

    Change FlagArtist to render an actual flag.

--- deli/artist/flag_label_artist.py
+++ deli/artist/flag_label_artist.py
@@ -4,7 +4,7 @@ from traits.api import HasStrictTraits, Instance
 from .label_artist import LabelArtist
 
 
-DEFAULT_LABEL_STYLE = {'x_origin': 'left', 'x_offset': 20}
+DEFAULT_LABEL_STYLE = {'x_origin': 'left', 'x_offset': 20, 'margin': 5}
 
 
 class FlagArtist(HasStrictTraits):
@@ -12,12 +12,25 @@ class FlagArtist(HasStrictTraits):
     fill_color = ColorTrait('yellow')
     edge_color = ColorTrait('black')
 
-    def draw(self, gc, rect):
+    def draw(self, gc, rect, origin=(0, 0)):
         with gc:
-            gc.set_stroke_color(self.edge_color_)
-            gc.set_fill_color(self.fill_color_)
-            gc.draw_rect([int(a) for a in rect])
-            gc.fill_path()
+            if self.fill_color is not 'none':
+                gc.set_fill_color(self.fill_color_)
+                gc.lines(self._poly_vertices(origin, rect))
+                gc.fill_path()
+            if self.edge_color is not 'none':
+                gc.set_stroke_color(self.edge_color_)
+                gc.lines(self._poly_vertices(origin, rect))
+                gc.close_path()
+                gc.stroke_path()
+
+    def _poly_vertices(self, origin, rect):
+        x, y, width, height = rect
+        return [origin,
+                (x, y),
+                (x + width, y),
+                (x + width, y + height),
+                (x, y + height)]
 
 
 class FlagLabelArtist(HasStrictTraits):
