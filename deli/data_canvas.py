5bbb03c83679fa6b185c9bf6adc5a6f4a8053336
Tony S Yu <tsyu80@gmail.com>
2014-05-16 00:36:14 -0500

    Consolidate PlotContainer and BasePlotContainer into DataCanvas and PlotCanvas

--- deli/data_canvas.py
+++ deli/data_canvas.py
@@ -1,128 +1,85 @@
-""" Defines the DataCanvas class, and associated property traits and property
-functions.
-"""
 import numpy as np
 
-from traits.api import Bool, Instance, Property
+from enable.api import Container
+from enable.simple_layout import (simple_container_get_preferred_size,
+                                  simple_container_do_layout)
+from traits.api import Instance, Property, Str
 
-from .axis import BaseAxis, XAxis, YAxis
-from .grid import BaseGrid, XGrid, YGrid
 from .layout.bbox_transform import BboxTransform
 from .layout.bounding_box import BoundingBox
-from .plot_containers import OverlayPlotContainer
+from .plot_component import DEFAULT_DRAWING_ORDER
 
 
-def replace_in_list(a_list, old, new):
-    if old in a_list:
-        a_list.remove(old)
-    if new is not None:
-        a_list.append(new)
-
-
-class DataCanvas(OverlayPlotContainer):
+class DataCanvas(Container):
     """ Represents a mapping from 2-D data space into 2-D screen space.
 
     It can house plots and other plot components, and otherwise behaves
-    just like a normal PlotContainer.
+    just like a normal Container.
     """
+
+    draw_order = Instance(list, args=(DEFAULT_DRAWING_ORDER,))
+    draw_layer = Str("plot")
+
+    # Do not use an off-screen backbuffer.
+    use_backbuffer = False
+
+    #--------------------------------------------------------------------------
+    #  Bounding box
+    #--------------------------------------------------------------------------
+
+    #: Bounding box in screen coordinates
+    screen_bbox = Instance(BoundingBox)
 
     # The bounding box containing data added to plot.
     data_bbox = Instance(BoundingBox)
 
+    def _screen_bbox_default(self):
+        return BoundingBox.from_extents(self.x, self.y, self.x2, self.y2)
+
     def _data_bbox_default(self):
         return BoundingBox.from_extents(np.inf, np.inf, -np.inf, -np.inf)
 
+    def _bounds_changed(self, old, new):
+        super(DataCanvas, self)._bounds_changed(old, new)
+        self._update_bbox()
+
+    def _position_changed(self, old, new):
+        super(DataCanvas, self)._position_changed(old, new)
+        self._update_bbox()
+
+    def _update_bbox(self):
+        self.screen_bbox.bounds = (self.x, self.y, self.width, self.height)
+        self.invalidate_draw()
+
+    #--------------------------------------------------------------------------
+    #  Transformations
+    #--------------------------------------------------------------------------
+
     #: Transform from data space to screen space.
     data_to_screen = Instance(BboxTransform)
-
-    def _data_to_screen_default(self):
-        return BboxTransform(self.data_bbox, self.screen_bbox)
 
     #: Transform from data space to screen space.
     screen_to_data = Property(Instance(BboxTransform),
                               depends_on='data_to_screen')
 
+    def _data_to_screen_default(self):
+        return BboxTransform(self.data_bbox, self.screen_bbox)
+
     def _get_screen_to_data(self):
         return self.data_to_screen.inverted()
 
-    #------------------------------------------------------------------------
-    # Axis and Grids
-    #------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
+    #  Layout
+    #--------------------------------------------------------------------------
 
-    # The horizontal axis.
-    x_axis = Instance(BaseAxis)
+    def get_preferred_size(self, components=None):
+        """ Returns the preferred size (width, height) for this component.
 
-    # The vertical axis.
-    y_axis = Instance(BaseAxis)
+        Overrides PlotComponent
+        """
+        return simple_container_get_preferred_size(self, components=components)
 
-    # The grid that intersects the x-axis, i.e., a set of vertical lines.
-    x_grid = Instance(BaseGrid)
-
-    # The grid that intersects the y-axis, i.e., a set of horizontal lines.
-    y_grid = Instance(BaseGrid)
-
-    # Whether to automatically create the x_axis and y_axis if they were not
-    # already set by the caller.
-    auto_axis = Bool(True)
-
-    # Whether to automatically create the x_grid and y_grid if they were not
-    # already set by the caller.
-    auto_grid = Bool(True)
-
-    #------------------------------------------------------------------------
-    # Appearance
-    #------------------------------------------------------------------------
-
-    bgcolor = "white"
-
-    # Padding defaults.
-    padding_top = 50
-    padding_bottom = 50
-    padding_left = 50
-    padding_right = 50
-
-    #------------------------------------------------------------------------
-    # Public methods
-    #------------------------------------------------------------------------
-
-    def __init__(self, **kwtraits):
-        super(DataCanvas, self).__init__(**kwtraits)
-        self._init_components()
-
-    #-------------------------------------------------------------------------
-    # Event handlers
-    #-------------------------------------------------------------------------
-
-    def _x_grid_changed(self, old, new):
-        self._replace_underlay(old, new)
-
-    def _y_grid_changed(self, old, new):
-        self._replace_underlay(old, new)
-
-    def _x_axis_changed(self, old, new):
-        self._replace_underlay(old, new)
-
-    def _y_axis_changed(self, old, new):
-        self._replace_underlay(old, new)
-
-    #------------------------------------------------------------------------
-    # Private methods
-    #------------------------------------------------------------------------
-
-    def _init_components(self):
-        if not self.x_grid and self.auto_grid:
-            self.x_grid = XGrid(component=self)
-        if not self.y_grid and self.auto_grid:
-            self.y_grid = YGrid(component=self)
-
-        if not self.x_axis and self.auto_axis:
-            self.x_axis = XAxis(component=self)
-
-        if not self.y_axis and self.auto_axis:
-            self.y_axis = YAxis(component=self)
-
-    def _replace_underlay(self, old, new):
-        replace_in_list(self.underlays, old, new)
-
-    def _replace_overlay(self, old, new):
-        replace_in_list(self.overlays, old, new)
+    def _do_layout(self):
+        """ Actually performs a layout (called by do_layout()).
+        """
+        simple_container_do_layout(self)
--- deli/plot_canvas.py
+++ deli/plot_canvas.py
@@ -1,18 +1,51 @@
 """ Defines the Plot class.
 """
-from traits.api import Dict, Instance, Str
+from traits.api import Bool, Dict, Instance, Str
 
+from .axis import BaseAxis, XAxis, YAxis
 from .data_canvas import DataCanvas
+from .grid import BaseGrid, XGrid, YGrid
 from .plot_label import PlotLabel
 from .plots.base_plot import BasePlot
 from .style import config
 from .utils.misc import new_item_name
 
 
+def replace_in_list(a_list, old, new):
+    if old in a_list:
+        a_list.remove(old)
+    if new is not None:
+        a_list.append(new)
+
+
 class PlotCanvas(DataCanvas):
     """ Represents a correlated set of data, plots, and axes in a single
     screen region.
     """
+
+    #------------------------------------------------------------------------
+    # Axis and Grids
+    #------------------------------------------------------------------------
+
+    # The horizontal axis.
+    x_axis = Instance(BaseAxis)
+
+    # The vertical axis.
+    y_axis = Instance(BaseAxis)
+
+    # The grid that intersects the x-axis, i.e., a set of vertical lines.
+    x_grid = Instance(BaseGrid)
+
+    # The grid that intersects the y-axis, i.e., a set of horizontal lines.
+    y_grid = Instance(BaseGrid)
+
+    # Whether to automatically create the x_axis and y_axis if they were not
+    # already set by the caller.
+    auto_axis = Bool(True)
+
+    # Whether to automatically create the x_grid and y_grid if they were not
+    # already set by the caller.
+    auto_grid = Bool(True)
 
     #------------------------------------------------------------------------
     # General plotting traits
@@ -29,6 +62,26 @@ class PlotCanvas(DataCanvas):
     title = Instance(PlotLabel)
 
     #------------------------------------------------------------------------
+    # Appearance
+    #------------------------------------------------------------------------
+
+    bgcolor = "white"
+
+    # Padding defaults.
+    padding_top = 50
+    padding_bottom = 50
+    padding_left = 50
+    padding_right = 50
+
+    #--------------------------------------------------------------------------
+    # Object interface
+    #--------------------------------------------------------------------------
+
+    def __init__(self, **kwtraits):
+        super(DataCanvas, self).__init__(**kwtraits)
+        self._init_components()
+
+    #------------------------------------------------------------------------
     # Public methods
     #------------------------------------------------------------------------
 
@@ -40,11 +93,49 @@ class PlotCanvas(DataCanvas):
         self.data_bbox.update_from_extents(*plot.data_extents)
         plot.data_bbox = self.data_bbox
 
+    #-------------------------------------------------------------------------
+    # Event handlers
+    #-------------------------------------------------------------------------
+
+    def _x_grid_changed(self, old, new):
+        self._replace_underlay(old, new)
+
+    def _y_grid_changed(self, old, new):
+        self._replace_underlay(old, new)
+
+    def _x_axis_changed(self, old, new):
+        self._replace_underlay(old, new)
+
+    def _y_axis_changed(self, old, new):
+        self._replace_underlay(old, new)
+
     #------------------------------------------------------------------------
-    # Private methods
+    # Traits defaults
     #------------------------------------------------------------------------
 
     def _title_default(self):
         title = PlotLabel(font=config.get('title.font'), component=self)
         self.overlays.append(title)
         return title
+
+    #------------------------------------------------------------------------
+    # Private methods
+    #------------------------------------------------------------------------
+
+    def _init_components(self):
+        if not self.x_grid and self.auto_grid:
+            self.x_grid = XGrid(component=self)
+        if not self.y_grid and self.auto_grid:
+            self.y_grid = YGrid(component=self)
+
+        if not self.x_axis and self.auto_axis:
+            self.x_axis = XAxis(component=self)
+
+        if not self.y_axis and self.auto_axis:
+            self.y_axis = YAxis(component=self)
+
+    def _replace_underlay(self, old, new):
+        replace_in_list(self.underlays, old, new)
+
+    def _replace_overlay(self, old, new):
+        replace_in_list(self.overlays, old, new)
