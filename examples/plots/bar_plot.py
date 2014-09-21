adbbc52f9be1f424297414ec8ded4399b58db8dc
Tony S Yu <tsyu80@gmail.com>
2014-09-21 12:14:25 -0500

    Change bar widths to data space

--- examples/plots/bar_plot.py
+++ examples/plots/bar_plot.py
@@ -7,7 +7,7 @@ from deli.axis import XAxis
 from deli.demo_utils.traitsui import TraitsWindow
 from deli.graph import Graph
 from deli.artist.base_point_artist import BasePointArtist
-
+from deli.utils.drawing import broadcast_points
 
 ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -24,23 +24,33 @@ class OrdinalAxis(XAxis):
 
 
 class BarArtist(BasePointArtist):
-    """ A plot for line data.
-    """
+    """ An artist for bar plot data. """
 
     stylus = Instance(RectangleStylus, ())
+
+    def draw(self, gc, view_rect=None):
+        bars = self._bars_from_points(width=0.5)
+        with self._clipped_context(gc):
+            for rect in bars:
+                self.stylus.draw(gc, rect)
 
     # -------------------------------------------------------------------------
     #  Private interface
     # -------------------------------------------------------------------------
 
-    def draw(self, gc, view_rect=None):
-        points = self.get_screen_points()
-        x0, y0 = self.data_to_screen.transform([0, 0])
-        with self._clipped_context(gc):
-            width = 20
-            for x, y in points:
-                rect = (x-width/2.0, y0, width, y - y0)
-                self.stylus.draw(gc, rect)
+    def _bars_from_points(self, width=None, y0=0):
+        x = self.x_data
+        y = self.y_data
+
+        if width is None:
+            width = np.min(np.diff(x))
+
+        corner0 = broadcast_points(x - (width / 2.0), y0)
+        corner1 = broadcast_points(x + (width / 2.0), y)
+
+        origins = self.data_to_screen.transform(corner0)
+        sizes = self.data_to_screen.transform(corner1) - origins
+        return np.hstack([origins, sizes])
 
 
 class Demo(TraitsWindow):
@@ -48,11 +58,10 @@ class Demo(TraitsWindow):
     def setup_graph(self):
         graph = Graph()
         graph.title.text = "Bar Artist"
-        graph.x_axis = OrdinalAxis(component=graph.canvas,
-                                   labels=ALPHA[:10])
+        graph.x_axis = OrdinalAxis(component=graph.canvas, labels=ALPHA[:10])
 
         x = np.arange(10)
-        artist = BarArtist(x_data=x, y_data=np.sin(x))
+        artist = BarArtist(x_data=x, y_data=np.cos(x))
         graph.add_artist(artist)
         return graph
 
